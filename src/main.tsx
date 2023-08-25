188805aaee76351038958476b349b5f5038e483c
pavarisk <p.kakanat@gmail.com>
2023-08-26 11:58:02 +1200

    Format documents

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <h1>Thousands Dice Game</h1>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
